79a0c835e0903006c4b8a79617992cfea986600a
nameaisy <nameshafa2@gmail.com>
2021-03-21 22:34:40 +0700

    Implementasi struktur kelas aplikasi kasir

---
+++ src/main/java/AplikasiRestauran/Menu.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package AplikasiRestauran;
+
+/**
+ *
+ * @author names
+ */
+public class Menu {
+
+private String nama_menu;
+
+private double harga;
+
+private String kategori;
+
+public String getNama_menu() { return "";}
+
+public void setNama_menu (String nama_menu){}
+
+public double getHarga () {return 0; }
+
+void setBarga (double harga){}
+
+public String getKategori() { return "";}
+
+public void setKategori (String kategori){} 
+}
---
+++ src/main/java/AplikasiRestauran/Pesanan.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package AplikasiRestauran;
+
+/**
+ *
+ * @author names
+ */
+public class Pesanan {
+
+private Menu menu;
+
+private int jumlah;
+
+private String keterangan;
+
+public Pesanan (Menu menu, int jumlah){}
+
+public void setketerangan (String keterangan) {}
+
+public String getKeterangan() { return "";}
+
+}
