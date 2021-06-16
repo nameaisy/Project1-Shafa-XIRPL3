7e116f4ea26a47f04ce8087462a96a4dce3d7a91
nameaisy <nameshafa2@gmail.com>
2021-06-17 06:18:30 +0700

    Modul D: Input Data Pelanggan

--- src/main/java/AplikasiRestauran/MainAplikasiKasir.java
+++ src/main/java/AplikasiRestauran/MainAplikasiKasir.java
@@ -33,9 +33,6 @@ public class MainAplikasiKasir {
 
         //mulai transaksi
         System.out.print("=========== TRANSAKSI ===========");
-
-        do {
-
             System.out.print("No Transaksi :  ");
             no_transaksi = input.next();
             System.out.print("Pemesan : ");
@@ -49,89 +46,6 @@ public class MainAplikasiKasir {
                 System.out.print("Nomor Meja : ");
                 no_meja = input.next();
             }
-
-            Transaksi trans = new Transaksi(no_transaksi, nama_pemesan, tanggal, no_meja);
-            System.out.println("=========== PESANAN ===========");
-            int no_kuah;
-            do {
-                Menu menu_yang_dipilih = app.daftarMenu.pilihMenu();
-                jumlah_pesanan = (int) app.cekInputNumber("Jumlah : ");
-
-                Pesanan pesanan = new Pesanan(menu_yang_dipilih, jumlah_pesanan);
-                trans.tambahPesanan(pesanan);
-
-                if (menu_yang_dipilih.getKategori().equals("Ramen")) {
-                    int jumlah_ramen = jumlah_pesanan;
-                    do {
-                        Menu kuah_yang_dipilih = app.daftarMenu.pilihKuah();
-
-                        System.out.print("Level : [0 - 5] : ");
-                        String level = input.next();
-
-                        int jumlah_kuah = 0;
-                        do {
-                            jumlah_kuah = (int) app.cekInputNumber("Jumlah : ");
-
-                            if (jumlah_kuah > jumlah_ramen) {
-                                System.out.println("[Err] Jumlah kuah melebihi jumlah ramen yang sudah dipesan");
-                            } else {
-                                break;
-                            }
-                        } while (jumlah_kuah > jumlah_ramen);
-
-                        Pesanan pesan_kuah = new Pesanan(kuah_yang_dipilih, jumlah_kuah);
-                        pesan_kuah.setKeterangan("Level " + level);
-                        trans.tambahPesanan(pesan_kuah);
-
-                        jumlah_ramen -= jumlah_kuah;
-                    } while (jumlah_ramen > 0);
-                } else {
-                    System.out.print("Keterangan [- jika kosong] : ");
-                    keterangan = input.next();
-                }
-
-                if (keterangan.equals("-")) {
-                    pesanan.setKeterangan(keterangan);
-                }
-
-                System.out.print("Tambah Pesanan lagi? [Y/N] : ");
-                pesan_lagi = input.next();
-            } while (pesan_lagi.equalsIgnoreCase("Y"));
-            trans.cetakStruk();
-
-            double total_pesan = trans.hitungTotalPesanan();
-            System.out.println("===========================");
-            System.out.println("Total : \t\t" + total_pesan);
-
-            trans.setPajak(PAJAK_PPN);
-            double ppn = trans.hitungPajak();
-            System.out.println("Pajak 10% : \t\t" + ppn);
-
-            double biaya_service = 0;
-            if (makan_ditempat.equalsIgnoreCase("Y")) {
-                trans.setBiayaService(BIAYA_SERVICE);
-                biaya_service = trans.hitungBiayaService();
-                System.out.println("Biaya Service 5% : \t" + biaya_service);
-            }
-
-            System.out.println("Total : \t\t" + trans.hitungTotalBayar(ppn, biaya_service));
-
-            double kembalian = 0;
-            do {
-                double uang_bayar = app.cekInputNumber("Uang Bayar: \t\t");
-
-                kembalian = trans.hitungKembalian(uang_bayar);
-                if (kembalian < 0) {
-                    System.out.println("[Err] Uang anda kurang");
-                } else {
-                    System.out.println("Kembalian : \t\t" + kembalian);
-                    break;
-                }
-            } while (kembalian < 0);
-
-            System.out.println("Lakukan Transaksi Lagi? [Y/N]");
-            transaksi_lagi = input.next();
-        }while (transaksi_lagi.equalsIgnoreCase("Y"));
 
         System.out.println("============ TERIMA KASIH ============");
 
